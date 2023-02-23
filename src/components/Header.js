4501c490a9125fd2ecc18a8ac016f8700effd622
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-23 15:38:39 -0800

    style header with bigger name and imported background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
 // import dependencies
 import React from 'react';
-
+// import images
+import headerBackground from '../images/headerBackground.jpg';
 // import user-defined React components
 import Navigation from './Navigation';
+
+// define styles for 'Header' component
+const styles = {
+  headerStyle: {
+    backgroundImage: `url(${headerBackground})`
+  },
+  nameStyle: {
+    fontSize: '3rem'
+  }
+};
 
 // define component 'Header' and export it as default
 export default function Header({ currentPage, handlePageChange }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark p-5" style={styles.headerStyle}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="#Heidi">Heidi Wu</a>
+        <a className="navbar-brand" href="#Heidi" style={styles.nameStyle}>Heidi Wu</a>
         <button className="navbar-toggler" 
           type="button" data-bs-toggle="collapse" 
           data-bs-target="#navbarSupportedContent" 
