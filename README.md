128568de923799c824289d8e3c8b7aabd3ece267
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-24 16:28:20 -0800

    build 'About Me' page: photo and bio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React-Portfolio
 A portfolio built with React to share my projects.
+
+Image by <a href="https://pixabay.com/users/davidzydd-985081/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2484120">David Zydd</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2484120">Pixabay</a>
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,12 +1,73 @@
+// import dependencies
 import React from 'react';
 
+// import images
+import aboutMeBackground from "../../images/aboutMeBackground.jpg";
+import aboutMePhoto from "../../images/aboutMePhoto.jpg";
+
+
+// define styles for 'AboutMe' component
+const styles = {
+  container: {
+    backgroundImage: `url(${aboutMeBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "repeat-y",
+  },
+  /* responsive for photo */
+  photoContainer: {
+    position: "relative",
+    width: "100%",
+    height: 0,
+    paddingBottom: "133.33%"
+  },
+  photo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+  bio: {
+    fontSize: "1.2rem",
+    backgroundColor: "rgba(255,255,255, 0.4)",
+    borderRadius: "10px",
+  }
+};
+
+// define component 'AboutMe' and export it as default
 export default function AboutMe() {
   return (
-    <div className="pt-5 px-5" style={{background:"lightgrey", height:"800px"}}>
-      <h1>About Me</h1>
-      <p>
-        Coming soon...
-      </p>
+    <div className="container-fluid p-5" style={styles.container}>
+      <div className="row" >
+        {/* title */}
+        <div className="col-12">
+          <h1 className='py-3'>
+            About Me
+          </h1>
+        </div>
+      </div>
+      <div className="row d-flex align-items-strech">
+        {/* photo */}
+        <div className="col-sm-5 col-lg-3 col-xl-2">
+          <div className="mb-4" style={styles.photoContainer}>
+            <img className="rounded" src={aboutMePhoto} alt="aboutMePhoto" style={styles.photo}/>
+          </div>
+        </div>
+        {/* bio */}
+        <div className="col-sm-12 col-lg-5 col-xl-5 d-flex align-items-strech" >
+          <div className="d-flex align-items-center px-4 mb-4" style={styles.bio}>
+            <p>
+              {`Hi, my name is Heidi Wu. 
+              I am a full-stack web developer with a background in computer science 
+              and a passion for creating beautiful and functional web applications. 
+              I am currently enrolled in the University of California San Diego's Full-Stack Web Development Bootcamp.
+              I am proficient in HTML, CSS, JavaScript, jQuery, Bootstrap, Node.js, Express.js, MySQL, MongoDB, React.js, and more.
+              I am looking for a full-time position as a web developer.`}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
