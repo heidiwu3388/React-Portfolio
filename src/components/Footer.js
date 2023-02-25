42b97a30a2b5afede3b31e843ec8642d8fda6be2
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-24 19:59:43 -0800

    build the 'Footer' component

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+// import dependencies
+import React from "react";
+// import images
+import footerBackground from "../images/footerBackground.jpg";
+import githubIcon from "../images/github-mark-white.svg";
+import linkedinIcon from "../images/linkedin.png";
+import stackoverflowIcon from "../images/stackoverflow.png";
+// define styles for 'Footer' component
+const styles = {
+  footerStyle: {
+    backgroundImage: `url(${footerBackground})`,
+  },
+  iconStyle: {
+    width: "50px",
+    height: "50px",
+  },
+};
+
+// define component 'Footer' and export it as default
+export default function Footer() {
+  console.log("inside Footer component");
+  return (
+    <div
+      className="container-flluid py-3"
+      style={styles.footerStyle}
+    >
+      <div className="row d-flex justify-content-center">
+        <div className="col-8 col-sm-6 col-lg-4 d-flex justify-content-around">
+          <a href="https://github.com/heidiwu3388" rel="noreferrer" target="_blank">
+            <img src={githubIcon} alt="github" style={styles.iconStyle}/>
+          </a>
+          <a href="https://www.linkedin.com/in/heidi-wu-lau/" rel="noreferrer" target="_blank">
+            <img src={linkedinIcon} alt="linkedin" style={styles.iconStyle}/>
+          </a>
+          <a href="https://stackoverflow.com/users/19934254/heidi-wu" rel="noreferrer" target="_blank">
+            <img src={stackoverflowIcon} alt="stackoverflow" style={styles.iconStyle}/>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 // import user-defined React components
 import Header from './Header';
+import Footer from './Footer';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
@@ -36,6 +37,7 @@ export default function PortfolioContainer() {
     <div>
       <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
       {renderPage()}
+      <Footer/>
     </div>
   );
 }
