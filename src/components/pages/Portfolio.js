a99c3d853316d895c0634a601912c469c6a59bd7
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-25 21:40:46 -0800

    build the 'Portfolio' page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,79 @@
+// import dependencies
 import React from 'react';
 
+// import user-defined React components
+import Project from './Project';
+
+// import images
+import portfolioBackground from '../../images/portfolioBackground.jpg';
+import evChargeNGo from '../../images/Ev-Charge-N-Go.jpg';
+import letsGoDIY from '../../images/Lets-Go-DIY.jpg';
+import techBlog from '../../images/Tech-Blog.jpg';
+import socialNetworkAPI from '../../images/Social-Network-API.jpg';
+import weatherDashboard from '../../images/Weather-Dashboard.jpg';
+import noteTaker from '../../images/Note-Taker.jpg';
+
+// define styles for 'Portfolio' component
+const styles = {
+  container: {
+    backgroundImage: `url(${portfolioBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "repeat-y",
+  },
+};
+
+// define component 'Portfolio' and export it as default
 export default function Portfolio() {
   return (
-    <div className="pt-5 px-5" style={{background:"lightgrey", height:"800px"}}>
-      <h1>Portfolio</h1>
-      <p>
-        Coming soon...
-      </p>
+    <div className="container-fluid px-4 px-sm-5 py-3" style={styles.container}>
+      {/* title */}
+      <div className="row" >
+        <div className="col-12 d-flex justify-content-center">
+          <h1 className='py-3 text-white'>
+            Portfolio
+          </h1>
+        </div>
+      </div>
+      {/* projects */}
+      <div className="row d-flex px-lg-5 justify-content-center align-items-stretch">
+        <Project
+          backgroundImage={evChargeNGo}
+          title="EV Change N Go"
+          githubLink="https://github.com/heidiwu3388/EV-Charge-N-Go"
+          deployLink="https://ev-charge-n-go.herokuapp.com/"
+        />
+        <Project
+          backgroundImage={letsGoDIY}
+          title="Let's Go DIY"
+          githubLink="https://github.com/heidiwu3388/Let-s-Go-DIY"
+          deployLink="https://heidiwu3388.github.io/Let-s-Go-DIY/"
+        />
+        <Project
+          backgroundImage={techBlog}
+          title="Tech Blog"
+          githubLink="https://github.com/heidiwu3388/Tech-Blog"
+          deployLink="https://heidi-tech-blog.herokuapp.com/"
+        />
+        <Project
+          backgroundImage={socialNetworkAPI}
+          title="Social Network API"
+          githubLink="https://github.com/heidiwu3388/Social-Network-API"
+          deployLink="https://www.youtube.com/watch?v=RAhfnYqq0Yw"
+        />
+        <Project
+          backgroundImage={weatherDashboard}
+          title="Weather Dashboard"
+          githubLink="https://github.com/heidiwu3388/Weather-Dashboard"
+          deployLink="https://heidiwu3388.github.io/Weather-Dashboard/"
+        />
+        <Project
+          backgroundImage={noteTaker}
+          title="Note Taker"
+          githubLink="https://github.com/heidiwu3388/Note-Taker"
+          deployLink="https://heidi-note-taker.herokuapp.com/"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/Project.js
@@ -0,0 +1,65 @@
+// import dependencies
+import React from 'react';
+
+// import images
+import githubIcon from '../../images/github-mark.svg';
+
+// define component 'Project' and export it as default
+export default function Project({ backgroundImage, title, githubLink, deployLink }) {
+  // define styles for 'Project' component
+  const styles = {
+    projectContainer: {
+      // keep the aspect ratio of the image
+      width: "100%",
+      height: 0,
+      paddingBottom: "66.66%",
+      // setup for absolute positioning of the child elements (title and githubIcon)
+      position: "relative",
+      // set the background image
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      boxShadow: "15px 15px 10px rgba(0,0,0,0.5)",
+    },
+    projectTitle: {
+      // position the title to lower left corner
+      position: "absolute",
+      bottom: "5px",
+      left: 0,
+      padding: "5px 10px 5px 10px",
+      // set the background color and opacity
+      backgroundColor: "rgba(000,000,000, 0.7)",
+      fontSize: "1.5rem",
+      color: "white",
+    },
+    githubIcon: {
+      // position the github icon to upper right corner
+      position: "absolute",
+      top: 0,
+      right: 0,
+      // set the size
+      width: "50px",
+      height: "50px",
+      // set the border radius to make it a circle
+      borderRadius: "50%",
+      backgroundColor: "rgba(225,225,225, 0.6)",
+      margin: "10px",
+      padding: "5px",
+    },
+
+  };
+  return (
+    <div className="col-sm-6 col-lg-4 mb-4">
+      <div className="" style={styles.projectContainer}>
+        <a href={githubLink} rel="noreferrer" target="_blank">
+          <img src={githubIcon} alt="projectImage" style={styles.githubIcon}/>
+        </a>
+        <a href={deployLink} rel="noreferrer" target="_blank">
+          <p style={styles.projectTitle}>
+            {title}
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+}
